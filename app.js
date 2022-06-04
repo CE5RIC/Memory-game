6fc109f8d5c7bfb73d74930b60f404c773870a21
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-04 16:27:57 +0200

    function created to shuffle cards

--- app.js
+++ app.js
@@ -1,47 +1,23 @@
-const section = document.querySelector("section");
-const playerLivesCount = document.querySelector("span");
-const playerLives = 5;
 
 
-
-// link playerlives to text
-playerLivesCount.textContent = playerLives;
-
-
-// STORE IMAGES IN AN ARRAY
-
-
-const arrayOfImages = [];
-
-arrayOfImages.push("Images/Astrologer copy.jpg",
-"Images/bandit copy.jpg",
-"Images/confessor copy.jpg",
-"Images/hero copy.jpg",
-"Images/Prisoner copy.jpg",
-"Images/prophet copy.jpg",
-"Images/samurai copy.jpg",
-"Images/vagabond copy.jpg",
-"Images/warrior copy.jpg",
-"Images/wretch copy.jpg","Images/Astrologer copy.jpg",
-"Images/bandit copy.jpg",
-"Images/confessor copy.jpg",
-"Images/hero copy.jpg",
-"Images/Prisoner copy.jpg",
-"Images/prophet copy.jpg",
-"Images/samurai copy.jpg",
-"Images/vagabond copy.jpg",
-"Images/warrior copy.jpg",
-"Images/wretch copy.jpg");
-
-
+function shuffleThem () {
+// Selecting all cards
+let allCards = document.querySelectorAll(".card");
+// Storing all cards in an array (Convert Nodelist to an array)
+let arrayCards = Array.from(allCards);
+ 
+console.log(arrayCards);
 
 // Randomize the cards
 
-const shuffledArray = arrayOfImages.sort((a, b) => 0.5 - Math.random());
+let shuffledCards = arrayCards.sort((a, b) => 0.5 - Math.random());
 
-console.log(shuffledArray);
+console.log(shuffledCards);
+
+}
 
 
+shuffleThem();
 
 
 
